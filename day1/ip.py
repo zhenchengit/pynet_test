a04199bcb1e3a5f654bb3e253b59c7f97c517e4b
Zhen Chen <zenkchen@gmail.com>
2016-07-18 15:50:59 -0700

    exercise

--- day1/ip.py
+++ day1/ip.py
@@ -1,4 +1,7 @@
 #!phthon 2
 ip = raw_input("please type ip :")
-print "{:<12} {:<12} {:<12} {:<12}".format(*ip.split('.'))
-
+# print "{:<12} {:<12} {:<12} {:<12}".format(*ip.split('.'))
+ip_addr = ip.split(".")
+ip_addr[3]='0'
+print (ip_addr[0],ip_addr[1],ip_addr[2],ip_addr[3])
+print (bin(int(ip_addr[0])),bin(int(ip_addr[1])),bin(int(ip_addr[2])),bin(int(ip_addr[3])))
---
+++ day1/list.py
@@ -0,0 +1,8 @@
+list = [0,1,2,3,4]
+print list
+list.append(5)
+list.append(6)
+print list
+list.pop(0)
+print list
+print len(list) 

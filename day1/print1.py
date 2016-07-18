18cbfa8460344a4663e29a37f62356f1f1ccc70f
Zhen Chen <zenkchen@gmail.com>
2016-07-18 14:11:45 -0700

    print1

---
+++ day1/print1.py
@@ -0,0 +1,8 @@
+var_s1 = 's1'
+var_s2 = 's2'
+var_s3 = 's3'
+print '{:>30}'.format(var_s1)
+print '{:>30}'.format(var_s2)
+print '{:>30}'.format(var_s3)
+var_s4 = raw_input('please s4:')
+print '{:>30}'.format(var_s4)
